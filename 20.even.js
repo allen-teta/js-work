75a7286b816c7e892c14b79cd1676a1989fced27
allen-teta <148436556+allen-teta@users.noreply.github.com>
2023-11-15 14:33:45 +0200

    even range

--- 20.even.js
+++ 20.even.js
@@ -1,11 +1,10 @@
 function evenNumbers(){
-    var sum=0;
     for(let i=0 ; i<=100 ; i++ ){
         if(i%2===0){
-            sum+=i;
+            console.log(i)
         }
 
     }
-    return sum;
+
 }
-console.log("sum = 1 and 100",evenNumbers());
+evenNumbers()
